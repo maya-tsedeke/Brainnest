97e15b4ab0722a1c8283de9b12bd2c00479c70c0
maya-tsedeke <32611349+maya-tsedeke@users.noreply.github.com>
2023-04-13 21:13:30 +0300

    Updated by Brainnest developer

--- Day3/task3.py
+++ Day3/task3.py
@@ -14,41 +14,39 @@ else:
     for line in lines:
         if line.startswith('From '):
             email = line.split()[1]
+            domain = email.split('@')[1]
             if email not in email_counts:
                 email_counts[email] = 1
             else:
                 email_counts[email] += 1
 
+            if domain not in email_counts:
+                email_counts[domain] = 1
+            else:
+                email_counts[domain] += 1
+
     if email_counts:
         max_email = max(email_counts, key=email_counts.get)
-        max_count = email_counts[max_email]
         print("Email address with the most occurrences:", max_email)
-        print("Number of occurrences:", max_count)
-
-        print("Email addresses and their occurrences:")
-        for email, count in email_counts.items():
-            print(email, count)
+        print("Number of occurrences:", email_counts[max_email])
+        print("Frequency of other domains:", email_counts)
     else:
         print("No email addresses found in the file.")"""
+#Answer from Brainnest developer team
+d = dict()
+fname = 'mbox-short.txt'
+fhand = open(fname)
+for line in fhand:
+    words = line.split()
+    if len(words) < 3 or words[0] != 'From': continue
+    else:
+        if words[1] not in d:
+            d[words[1]] = 1
+        else:
+            d[words[1]] += 1
 
-"""Answer updated from Brainnest Developer"""
-lst = []
-addresses = []
+print(d)
 
-try:
-    file = open('mbox-short.txt', 'r')
-    for line in file:
-        lst = line.split(' ')
-        if lst[0].startswith('From'):
-            lst = lst[1].split('@')
-            addresses.append(lst[1].strip())
-except FileNotFoundError:
-    pass
-
-frequencies = dict()
-
-for address in addresses:
-    frequencies[address] = addresses.count(address)
-print(frequencies)
-
-# print(max(addresses, key=addresses.count))
+max_value = max(d.values())
+key = max(d, key=d.get)
+print(key + ': ' + str(max_value))
